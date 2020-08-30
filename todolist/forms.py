b8f0d91898502276d7d1f3caa6b8a437b3a721e1
dcos96038 <diegocoscolla@gmail.com>
2020-08-30 20:12:25 -0300

    Cambios de aspecto en el front end

--- todolist/forms.py
+++ todolist/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class TodoListForm(forms.Form):
     text = forms.CharField(max_length=100,
         widget = forms.TextInput(
-            attrs={'class':'form-control', 'placeholder':'Descripcion de tarea', 'style':'width:400px', 'aria-label':'Todo', 'aria-describedby':'add-btn'},
+            attrs={'class':'form-control', 'placeholder':'Descripcion de tarea', 'style':'width:400px', 'aria-label':'Todo', 'aria-describedby':'add-btn', 'autofocus':True},
         ))
